899fd0a2d1f5a62fe5d0551ea90362fbb9fe1fe7
patrictobola <tobola92@gmail.com>
2023-08-02 14:49:55 +0200

    setted header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
@@ -10,9 +10,30 @@
 
 <body>
     <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-secondary">
+            <div class="container">
 
+                <a class="navbar-brand" href="https://getbootstrap.com/">Sponsored by Bootstrap</a>
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </header>
-    <h1>{{ $greet }}</h1>
+    <main>
+        <div class="container mt-5">
+            <h1>{{ $greet }}</h1>
+        </div>
+
+    </main>
 </body>
 
 </html>
